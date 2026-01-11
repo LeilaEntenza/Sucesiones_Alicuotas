ef89d494744cdb06482b9dece87d3b0406d6926a
Lei <leilachiderski@gmail.com>
2026-01-11 20:36:53 -0300

    Proyecto creado y empezado

---
+++ Sucesiones_Alicuotas.py
@@ -0,0 +1,3 @@
+# ¡Hola!, este repositorio está destinado a la divulgación científica y la eficiencia en la resolución de sucesiones alícuotas.
+while (num <= 0):
+    num = int(input("Ingrese un número"))
