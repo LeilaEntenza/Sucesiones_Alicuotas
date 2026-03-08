8f1ec7a3ab8c1a117c7e8c9ab8c6fe68a6aae98b
Lei <leilachiderski@gmail.com>
2026-03-08 20:12:00 -0300

    divisores

--- Sucesiones_Alicuotas.py
+++ Sucesiones_Alicuotas.py
@@ -1,3 +1,11 @@
 # ¡Hola!, este repositorio está destinado a la divulgación científica y la eficiencia en la resolución de sucesiones alícuotas.
+num = -1
 while (num <= 0):
     num = int(input("Ingrese un número"))
+
+divisores = []
+if num != 0:
+    for i in range (num, 1):
+        if num%i==0:
+            divisores[len(divisores)] = i
+            print(i + " es divisor")
